9622bf4109633824e503d7ff5d558a2902c5c2c1
Mike Barberry <mikebarberry@protonmail.com>
2020-10-08 01:52:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,7 @@
 def roll_call_dwarves(array)
   array.each_with_index do |element, index|
-    puts "#{index} #{{element}}""
+    puts "#{index} #{{element}}"
+  end 
 end
 
 def summon_captain_planet# code an argument here
