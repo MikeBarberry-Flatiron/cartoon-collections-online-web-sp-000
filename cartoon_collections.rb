f7911d20b78c9263cbf9fa096aadb9301d144553
Mike Barberry <mikebarberry@protonmail.com>
2020-10-08 02:07:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,11 +11,11 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  if array.length >= 4
-    true
-  else
+  if array.length =< 4
     false
-  end 
+  else
+    true
+  end
 end
 
 def find_the_cheese# code an argument here
